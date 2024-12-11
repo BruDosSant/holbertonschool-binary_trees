28c2b6373bd432c2a7d918584950bdfb8ec72e66
luxxy0 <lulydiaz2801@gmail.com>
2024-12-11 22:52:29 +0000

    binary_tree_insert_right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,27 +7,27 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node;
+	binary_tree_t *node;
 
-    if (!parent)
-        return (NULL);
+	if (!parent)
+		return (NULL);
 
-    node = malloc(sizeof(binary_tree_t));
-    if (!node)
-        return (NULL);
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
 
-    node->n = value;
-    node->parent = parent;
-    node->left = NULL;
-    node->right = NULL;
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
 
-    if (parent->right != NULL)
-    {
-        node->right = parent->right;
-        parent->right->parent = node;
-    }
+	if (parent->right != NULL)
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+	}
 
-    parent->right = node;
+	parent->right = node;
 
-    return (node);
+	return (node);
 }

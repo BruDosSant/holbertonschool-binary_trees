481f9d63c947e957f1a6635d345400eab10d697f
root <brudossant@gmail.com>
2024-12-15 17:15:01 +0000

    correccion documentacion 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,30 +1,37 @@
 #include "binary_trees.h"
 
+/**
+ * messi - mide altura de ambos lados del arbol
+ * @tree: pointer al primer nodulo del arbol
+ * Return: altura
+ */
+
 int messi(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
+	int left, right;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return (0);
+	if (tree == NULL)
+		return (-1);
 
-	left += messi(tree->left);
-	right += messi(tree->right);
+	left = messi(tree->left);
+	right = messi(tree->right);
 
 	if (left > right)
 		return (left + 1);
-
 	else
 		return (right + 1);
 }
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer al primer nodo del arbol
+ * Return: balance factor
+ */
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	else if (tree->left == NULL || tree->right == NULL)
-		return (0);
-
-	else
-		return (messi(tree->left) - messi(tree->right));
+	return (messi(tree->left) - messi(tree->right));
 }
